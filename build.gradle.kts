5491825ea62e2f628b034475c739b78eac3e4878
Yeseo Kim <chinkl850@gmail.com>
2025-12-20 21:58:16 +0900

    chore: kotest, mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,6 +74,9 @@ dependencies {
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.mockk:mockk:1.14.6")
 }
 
 kotlin {
